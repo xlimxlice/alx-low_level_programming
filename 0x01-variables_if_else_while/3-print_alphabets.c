12f0ab68aed2d40c54ca63cc0ef9641eff1199ae
xlimxlice <ddoupoylad@gmail.com>
2022-06-06 12:38:28 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: End the program
+ */
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; ++letter)
+putchar("letter);
+putchar("\n");
+return (0);
+}
