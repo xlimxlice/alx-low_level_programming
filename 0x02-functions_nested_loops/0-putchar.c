3bc72e51cf2b47e684e0bc1036ad4899e6c6bb3f
xlimxlice <ddoupoylad@gmail.com>
2022-06-08 13:25:11 -0700

    task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,16 +6,13 @@
  */
 int main(void)
 {
-README.md char string[8] = "_putchar";
-README.md int n = 0;
+char string[8] = "_putchar";
+int n = 0;
 
-README.md while (n < 8)
-README.md {
-README.md README.md _putchar(string[n]);
-README.md README.md ++n;
-README.md }
-
-README.md _putchar(10);
-README.md return (0);
-
+while (n < 8)
+{_putchar(string[n]);
+++n;
+}
+_putchar(10);
+return (0);
 }
