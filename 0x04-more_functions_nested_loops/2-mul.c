a94e19066b146681f4df8eccccdd076cebc74043
xlimxlice <ddoupoylad@gmail.com>
2022-06-10 21:31:32 -0700

    task 1

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *  * mul - function
- *   *
- *    * @a: parameter 1
- *     * @b: parameter 2
- *      *
- *       * Return: return value
- *        */
+* mul - function
+*
+* @a: parameter 1
+* @b: parameter 2
+*
+*Return: return value
+*/
 int mul(int a, int b)
 {
 		return (a * b);
