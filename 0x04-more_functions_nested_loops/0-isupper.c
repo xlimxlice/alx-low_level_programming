f3cde5cc1fe57ada05c9b69b54c5b64dded28fbc
xlimxlice <ddoupoylad@gmail.com>
2022-06-10 20:49:22 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,17 @@
-#include "main.h"
+nclude "main.h"
 /**
- * main - main function
- *
- * Return: End program
- */
+ *  * main - main function
+ *   *
+ *    * Return: End program
+ *     */
 int main(void)
 {
-README.md char string[8] = "_putchar";
-README.md int n = 0;
-
-README.md while (n < 8)
-README.md {
-README.md README.md _putchar(string[n]);
-README.md README.md ++n;
-README.md }
-
-README.md _putchar(10);
-README.md return (0);
-
+char string[8] = "_putchar";
+	int n = 0;
+	while (n < 8)
+{
+	_putchar(string[n]);								++n;
+}
+	_putchar(10);
+		return (0);
 }
