c36fd9c0205db9d1b923b76398742a18d7553ca0
xlimxlice <ddoupoylad@gmail.com>
2022-06-10 21:43:54 -0700

    task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- *  * print_numbers - function
- *   */
+* print_numbers - function
+*/
 void print_numbers(void)
 {
 		int n = 48;
 
 			while (n < 58)
-					{
-								_putchar(n++);
+			{
+				_putchar(n++);
 									}
 				_putchar(10);
 }
